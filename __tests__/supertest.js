da5c0ab6c1ea86638b74ae5c00ec9ce7bd19d257
sifulsidd <sifulsiddiki@gmail.com>
2023-08-16 09:26:33 -0400

    Added some test cases for the database authentication routes

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,25 +1,64 @@
-import request from 'supertest';
+const request = require('supertest')
 // import path from 'path';
 // import assert from 'assert';
-import fs from 'fs';
+// import fs from 'fs';
 
 
 const server = 'http://localhost:3000';
+const db = require('../server/models/dbModel');
 
-describe ('POST to sign up route', () => {
-    
-    it ('should add a new account to database', async () => {
+describe('POST to sign up route', () => {
+
+    it('should add a new account to database', async () => {
         // create test user account to be submitted
         const user = {
-        "email": "testuser@test.com",
-        "password": "12345"
+            "email": "testuser@test.com",
+            "password": "12345"
         };
-        
-    const response = await request(server)
-        .post ('/users')
-        .send(user)
-    
-    expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty('id');
+
+        const response = await request(server)
+            .post('/users')
+            .send(user)
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('id');
     })
 });
+
+describe('POST to login route', () => {
+    async function deleteTest() {
+        try{
+            const query = 'DELETE FROM users WHERE username = $1'
+            await db.query(query, ['testuser@test.com']);
+            console.log('Data should be deleted from table')
+        }
+        catch(err){
+            console.log('Could not delete test data');
+        }
+    }
+    it('should login successfully', async () => {
+        const user = {
+            "email": "testuser@test.com",
+            "password": "12345"
+        };
+        const response = await request(server)
+            .post('/users/login')
+            .send(user)
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('email');
+        expect(response.body).toHaveProperty('password');
+
+    });
+
+    it('should login unsuccessfully', async () => {
+        const user = {
+            "email": "failingtest123456@test.com",
+            "password": "failingtest123456"
+        };
+        const response = await request(server)
+            .post('/users/login')
+            .send(user)
+        expect(response.status).toBe(500);
+    });
+})
