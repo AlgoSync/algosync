0d98723601e27c552dd5932a61eaae3b8d258361
Joie Zhang <joie@princeton.edu>
2023-08-16 13:36:36 -0700

    dbModel.js was a testing file

--- server/models/dbModel.js
+++ server/models/dbModel.js
@@ -1,1 +1,4 @@
-export default {};
+const url = 'https://leetcode.com/problems/roman-to-integer/'
+// const problem_slug = url.match(/\/([^/]+)\/$/)[1];
+const problem_slug = url.match(/\/([^/]+)\/$/);
+console.log(problem_slug);
